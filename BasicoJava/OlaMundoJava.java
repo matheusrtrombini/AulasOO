2f578ae5a86caff79b884d753b2d5a53c81a4447
Matheus R. Trombini <matheus.trombini95@outlook.com>
2020-03-06 20:23:45 -0300

    teste

---
+++ BasicoJava/OlaMundoJava.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva a descrição da classe OlaMundoJava aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class OlaMundoJava
+{
+   public static void main(String[] args){
+       System.out.println("Meu primeiro código em Java");
+       
+    }
+}
